bca2f3828151d0c8d42e35edea92580f30e15c4e
Hussain Sajid <104356871+hussain-qwest@users.noreply.github.com>
2022-07-04 10:43:19 +0100

    Add files via upload

---
+++ councillor.js
@@ -0,0 +1,49 @@
+class councillorWidget {
+    constructor(town) {
+        this.town = town
+    }
+
+    getCouncillor () {
+        if (this.town === '') {
+            $('#councillor-content').empty()
+            return
+        }
+
+        $('#councillor-content').empty().append('<p>Retrieving information...</p>')
+
+        const payload = {
+            "town": {
+                "name": "town",
+                "value": this.town
+            }
+        }
+
+        runLookup('609b918c7dd6d', payload).then((response) => {            
+            if (response.success) {
+                this.councillor = response.data[0].councillor
+                this.displayResults()
+            } else {
+                this.displayError()
+            }
+        })
+    }
+
+    displayResults () {
+        let content = ''
+        content += '<div class="councillor-content-picture">'
+        content += '<img src="https://apps-cheshire-east.s3.amazonaws.com/staging/Waste/service-icons/food.png" alt="Councillor potrait">'
+        content += '</div>'
+        content += '<div class="councillor-content-name">'
+        content += '<p>' + this.name + '</p>'
+        content += '</div>'
+
+        $('#councillor-content').empty().append(content)
+    }
+
+    //Display the error message if data isn't returned properly
+    displayError = () => {
+        logIntRun()
+
+        $('#councillor-content').empty().append('<p>Your local councillor information is not available at the moment. Please try again later.</p>')
+    }
+}
